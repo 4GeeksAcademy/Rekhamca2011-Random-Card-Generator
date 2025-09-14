e918b24fbd5fab46926c188fca9990717bbe93b6
Rekhamca2011 <rekhamca2011@gmail.com>
2025-09-14 04:24:05 +0000

    random card generator function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector('.card');
+  document.querySelector('#width').addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
+      var inputWidth = document.querySelector('#width').value;
+      card.style.width = inputWidth + "px";
+      console.log(inputWidth);
+    }
+    if (e.key === 'Tab') {
+      var inputWidth = document.querySelector('#width').value;
+      card.style.width = inputWidth + "px";
+      console.log(inputWidth);
+    }
+  });
+  document.querySelector('#height').addEventListener("keypress", function (e) { 
+    if (e.key === 'Enter') {
+      var inputHeight = document.querySelector('#height').value;
+      card.style.height = inputHeight + "px";
+    }
+    if (e.key === 'Tab') {
+      var inputHeight = document.querySelector('#height').value;
+      card.style.height = inputHeight + "px";
+    }
+  });
+  document.querySelector('.top-suit').innerHTML = generateRandomSuit();
+  document.querySelector('.bottom-suit').innerHTML = document.querySelector('.top-suit').innerHTML; 
+  document.querySelector('.number').innerHTML = generateRandomNumber();
+  let counter = 10;
+  setInterval(function(){
+    document.querySelector('#timer').innerHTML = counter;
+    counter--;
+    if (counter === 0) {
+      window.location.reload();
+    }
+  }, 1000);
 };
+
+let generateRandomNumber = () => {                                  
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexNum = Math.floor(Math.random()*numbers.length);
+  return numbers[indexNum];
+}
+
+let generateRandomSuit = () => {                                   
+  let suit = ["♦", "♥", "♠", "♣"];
+  let indexSuit = Math.floor(Math.random()*suit.length);
+  if (indexSuit === 2 || indexSuit === 3) {
+    document.querySelector('.top-suit').style.color = "red";
+    document.querySelector('.bottom-suit').style.color = "red";
+  }
+  return suit[indexSuit];
+}
